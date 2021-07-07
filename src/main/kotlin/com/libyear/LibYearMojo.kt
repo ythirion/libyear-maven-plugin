8c66446177bffda722e823a9d9d235c1e10b9f05
Yoan Thirion <yoan.thirion@gmail.com>
2021-07-07 17:43:36 +0200

    Add detailed versions

--- src/main/kotlin/com/libyear/LibYearMojo.kt
+++ src/main/kotlin/com/libyear/LibYearMojo.kt
@@ -9,36 +9,70 @@ import org.codehaus.mojo.versions.rewriting.ModifiedPomXMLEventReader
 
 @Mojo(name = "run")
 class LibYearMojo: AbstractVersionsUpdaterMojo() {
-    private fun Dependency?.description(): String = "${this?.groupId}:${this?.artifactId}"
-    private fun ArtifactVersions?.latest(): String = this?.getNewestUpdate(this.currentVersion, UpdateScope.ANY).toString()
-    private fun ArtifactVersions?.diffWithCurrentVersion(): Int = this?.getAllUpdates(this.currentVersion, UpdateScope.ANY)?.size !!
-
-    private data class DependencyWithUpdate(
-        val name: String,
-        val projectVersion: String,
-        val latestVersion: String,
-        val versionDiff: Int) {
-        override fun toString(): String = "${this.name} ${this.projectVersion} -> ${this.latestVersion} (${this.versionDiff} versions behind)"
-    }
-
     override fun execute() {
         logResult(retrieveDependenciesWithUpdates())
     }
 
     private fun retrieveDependenciesWithUpdates(): List<DependencyWithUpdate> {
         return helper.lookupDependenciesUpdates(project.dependencies.toMutableSet(), false)
-            .mapNotNull { (artifact, versions) -> DependencyWithUpdate(artifact.description(), artifact.version, versions.latest(), versions.diffWithCurrentVersion())}
-            .filter { it.versionDiff > 0 }
+            .mapNotNull { (artifact, versions) ->
+                DependencyWithUpdate(
+                    artifact.description(),
+                    artifact.version,
+                    versions.latest(),
+                    versions.diffWithCurrentVersion()
+                )
+            }
+            .filter { it.versionDiff.any > 0 }
     }
 
     private fun logResult(dependenciesWithUpdates: List<DependencyWithUpdate>) {
-        if(dependenciesWithUpdates.any()) {
+        if (dependenciesWithUpdates.any()) {
             log.info("The following dependencies have newer versions:")
             dependenciesWithUpdates.forEach { log.info(it.toString()) }
-            log.info("You are ${dependenciesWithUpdates.sumOf { it.versionDiff }} versions behind for ${dependenciesWithUpdates.size} dependencies")
+            log.info(dependenciesWithUpdates.aggReport())
         } else {
-            log.info("No dependencies updates found. WELL DONE !!!")
+            log.info("No dependency updates found. WELL DONE !!!")
         }
+    }
+
+    //region extensions
+
+    private fun Dependency?.description(): String = "${this?.groupId}:${this?.artifactId}"
+    private fun ArtifactVersions?.latest(): String =
+        this?.getNewestUpdate(currentVersion, UpdateScope.ANY).toString()
+
+    private fun ArtifactVersions?.diffWithCurrentVersion(): VersionDiff =
+        VersionDiff(
+            this.diffWithCurrentVersion(UpdateScope.ANY),
+            this.diffWithCurrentVersion(UpdateScope.MAJOR),
+            this.diffWithCurrentVersion(UpdateScope.MINOR),
+            this.diffWithCurrentVersion(UpdateScope.INCREMENTAL) + this.diffWithCurrentVersion(UpdateScope.SUBINCREMENTAL),
+        )
+
+    private fun ArtifactVersions?.diffWithCurrentVersion(scope: UpdateScope): Int =
+        this?.getAllUpdates(currentVersion, scope)?.size!!
+
+    private fun List<DependencyWithUpdate>.aggReport(): String =
+        "> ${sumOf { it.versionDiff.any }} versions behind for $size dependencies" +
+                "-> ${sumOf { it.versionDiff.major }} major(s) | ${sumOf { it.versionDiff.minor }} minor(s) | ${sumOf { it.versionDiff.fixes }} fix(es)"
+
+    //endregion
+
+    private data class VersionDiff(val any: Int, val major: Int, val minor: Int, val fixes: Int) {
+        override fun toString(): String {
+            return "$any version(s) behind -> $major major(s) | $minor minor(s) | $fixes fix(es)"
+        }
+    }
+
+    private data class DependencyWithUpdate(
+        val name: String,
+        val projectVersion: String,
+        val latestVersion: String,
+        val versionDiff: VersionDiff
+    ) {
+        override fun toString(): String =
+            "${this.name} ${this.projectVersion} -> ${this.latestVersion} (${this.versionDiff})"
     }
 
     override fun update(pom: ModifiedPomXMLEventReader?) {
